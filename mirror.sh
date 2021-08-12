ccb7f152f3969291a092c9f801f778f4e5d75638
Aaron Abbott <aabmass@gmail.com>
2021-08-12 19:19:23 -0400

    update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-wget --page-requisites --convert-links --adjust-extension --mirror --span-hosts --content-disposition --domains=cancerresearchkenya.org https://cancerresearchkenya.org/
+wget https://cancerresearchkenya.org/ \
+     --page-requisites \
+     --convert-links \
+     --adjust-extension \
+     --mirror \
+     --span-hosts \
+     --content-disposition \
+     --domains=cancerresearchkenya.org \
+     --exclude-directories=wp-content/uploads/essential-addons-elementor
